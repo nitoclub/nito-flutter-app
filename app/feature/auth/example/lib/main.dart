59e71948e2d75ed89bb368b595c218ff4623fcc4
Daichi Aoki <fremie.off@gmail.com>
2022-10-17 23:49:18 +0900

    :bug: ログインページ単体の実行を可能に修正
    auth_exampleでbuild可能にしました。

--- app/feature/auth/example/lib/main.dart
+++ app/feature/auth/example/lib/main.dart
@@ -3,23 +3,16 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:feature_auth/auth.dart';
 
 void main() {
-  runApp(const LoginPage());
+  runApp(const LoginPageTest());
 }
 
-class MyApp extends HookConsumerWidget{
-  const MyApp({super.key});
+class LoginPageTest extends HookConsumerWidget{
+  const LoginPageTest({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: const Center(
-          child: Text('test'),
-        ),
-      ),
+    return const MaterialApp(
+      home: LoginPage(),
     );
   }
 }
--- app/feature/auth/example/test/widget_test.dart
+++ app/feature/auth/example/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:feature_auth_example/main.dart';
 void main() {
   testWidgets('Verify Platform version', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const LoginPageTest());
 
     // Verify that platform version is retrieved.
     expect(
