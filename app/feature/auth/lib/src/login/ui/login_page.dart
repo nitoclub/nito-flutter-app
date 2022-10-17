2a63a84c02d026c533cdfdd23e603baaedfbd651
Daichi Aoki <fremie.off@gmail.com>
2022-10-17 23:50:10 +0900

    :art: ログインページの側を実装
    ハリボテなので、今後controllerとstateが必要。

--- app/feature/auth/lib/src/login/ui/login_page.dart
+++ app/feature/auth/lib/src/login/ui/login_page.dart
@@ -8,10 +8,52 @@ class LoginPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Plugin example app'),
+        title: const Text('Welcome to Nito'),
       ),
-      body: const Center(
-        child: Text('test'),
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).primaryColor,
+                    width: 2.0,
+                  ),
+                ),
+                labelStyle: TextStyle(
+                  fontSize: 16,
+                  color: Theme.of(context).hintColor,
+                ),
+                labelText: 'Enter your e-mail',
+                floatingLabelStyle: const TextStyle(fontSize: 20),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).primaryColor,
+                    width: 1.0,
+                  ),
+                ),
+              ),
+              // controller: plz, create a textEditing controller,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 50),
+                backgroundColor: Theme.of(context).primaryColor,
+                disabledBackgroundColor: Theme.of(context).disabledColor,
+                shape: const StadiumBorder(),
+              ),
+              onPressed: () {
+                // event handling
+              },
+              child: const Text('Login'),
+            ),
+          ],
+        ),
       ),
     );
   }
