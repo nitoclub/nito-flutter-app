b484d0adf5725b8455ba12d21ec5932a9ced602a
Ryo Takeuchi <develop@r09i.me>
2022-10-19 07:21:43 +0900

    :sparkles: Supabase の初期化処理を追加
    Supabase の初期化処理を追加しました。
    初期化時に必要な情報を `.env` から読み込むため、以下のライブラリも併せて導入しています。
    
    - [flutter_dotenv](https://pub.dev/packages/flutter_dotenv)

--- README.md
+++ README.md
@@ -24,6 +24,17 @@ make prepare
 
 Supabase の セットアップ時に `Enter your database password:` と問われた場合は [こちらの Wiki](https://www.notion.so/2rabs/Supabase-5e6a3dcca45d4345839515eb7058c608) から取得してください。
 
+### 3. `.env`
+
+`/app/frontend` ディレクトリ配下にある `.env.example` を参考に `.env` ファイルを作成します。
+
+```text
+SUPABASE_URL=YOUR_SUPABASE_URL
+SUPABASE_ANON_KEY=YOUR_SUPABASE_ANON_KEY
+```
+
+- Supabase の情報は [こちら](https://app.supabase.com/project/hwxxihvcszfhaxlguajv/settings/api) から取得できます。
+
 ## Add Packages
 
 ### Package
--- app/frontend/lib/main.dart
+++ app/frontend/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:app/src/utils/url_strategy.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'src/app.dart';
 import 'src/settings/settings_controller.dart';
@@ -16,6 +18,15 @@ void main() async {
 
   // NOTE: Webページで '#' が混在するのを防ぐ
   usePathUrlStrategy();
+
+  // NOTE: env ファイルの読み込み
+  await dotenv.load();
+
+  // Supabase の初期化
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL'],
+    anonKey: dotenv.env['SUPABASE_ANON_KEY'],
+  );
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
