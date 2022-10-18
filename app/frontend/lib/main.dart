33f2691c6cd7fbcea9fe1e90291a4575668dbeb5
Ryo Takeuchi <develop@r09i.me>
2022-10-19 07:03:53 +0900

    :sparkles: Webビルド時に `#` が混在しないように変更
    Webビルド時に `#` が混在しないように変更しました。
    
    参考リンク
    - https://cbtdev.net/flutter-web-path-url-strategy/

--- app/frontend/lib/main.dart
+++ app/frontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app/src/utils/url_strategy.dart';
 import 'package:flutter/material.dart';
 
 import 'src/app.dart';
@@ -12,6 +13,9 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
+
+  // NOTE: Webページで '#' が混在するのを防ぐ
+  usePathUrlStrategy();
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
