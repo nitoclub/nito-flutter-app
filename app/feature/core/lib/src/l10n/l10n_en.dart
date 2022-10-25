2b5b06dab30db82772f1172c90567f65645233bf
Ryo Takeuchi <develop@r09i.me>
2022-10-25 11:19:39 +0900

    :bug: 生成された L10n クラスを追加
    生成された L10n クラスを追加しました。

---
+++ app/feature/core/lib/src/l10n/l10n_en.dart
@@ -0,0 +1,9 @@
+import 'l10n.dart';
+
+/// The translations for English (`en`).
+class L10nEn extends L10n {
+  L10nEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'NITO';
+}
---
+++ app/feature/core/lib/src/l10n/l10n_ja.dart
@@ -0,0 +1,9 @@
+import 'l10n.dart';
+
+/// The translations for Japanese (`ja`).
+class L10nJa extends L10n {
+  L10nJa([String locale = 'ja']) : super(locale);
+
+  @override
+  String get appTitle => 'NITO';
+}
