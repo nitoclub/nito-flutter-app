85401ecd141de1839e891e849d74ebf2513f0257
Ryo Takeuchi <develop@r09i.me>
2022-11-04 21:59:18 +0900

    :art: Nito で利用するアイコンをクラス化
    Nito で利用するアイコンのみをクラスでまとめました。

--- app/core/styleguide/lib/nito_styleguide.dart
+++ app/core/styleguide/lib/nito_styleguide.dart
@@ -1,3 +1,9 @@
+/// Component
 export 'src/component/nito_app_bar.dart';
 export 'src/component/nito_text_form_field.dart';
+
+/// Icon
+export 'src/icon/nito_icons.dart';
+
+/// Theme
 export 'src/theme/space.dart';
---
+++ app/core/styleguide/lib/src/icon/nito_icons.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class NitoIcons {
+  static const IconData email = Icons.email_outlined;
+  static const IconData password = Icons.password_outlined;
+}
