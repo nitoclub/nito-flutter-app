16137d61babef0114b7e1cf421dc1c8fe13d0c28
Ryo Takeuchi <develop@r09i.me>
2022-11-04 23:23:55 +0900

    :art: NITO 共通で利用する Scaffold コンポーネントを定義
    NITO 共通で利用する Scaffold コンポーネントを定義しました。

--- app/core/styleguide/lib/nito_styleguide.dart
+++ app/core/styleguide/lib/nito_styleguide.dart
@@ -1,6 +1,7 @@
 /// Component
 export 'src/component/nito_app_bar.dart';
 export 'src/component/nito_text_form_field.dart';
+export 'src/component/nito_scaffold.dart';
 
 /// Icon
 export 'src/icon/nito_icons.dart';
---
+++ app/core/styleguide/lib/src/component/nito_scaffold.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NitoScaffold extends Scaffold {
+  const NitoScaffold({
+    super.key,
+    PreferredSizeWidget? appBar,
+    Widget? body,
+  }) : super(
+          appBar: appBar,
+          body: body,
+        );
+}
