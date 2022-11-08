a1b3fd07430901099f9f8ace8e5188d4f5540de5
Ryo Takeuchi <develop@r09i.me>
2022-11-08 22:02:46 +0900

    :recycle: ログインフォーム部分をコンポーネントとして切り分け
    ログインフォーム部分をコンポーネントとして切り分けました。

---
+++ app/feature/auth/lib/src/login/component/login_form.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:nito_styleguide/nito_styleguide.dart';
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    super.key,
+    required this.isSignInProcessing,
+    required this.emailController,
+    required this.passwordController,
+    required this.onPasswordFieldSubmitted,
+    required this.onSignInButtonPressed,
+  });
+
+  final bool isSignInProcessing;
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
+  final void Function() onPasswordFieldSubmitted;
+  final void Function() onSignInButtonPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        // メールアドレス入力欄
+        TextFormField(
+          controller: emailController,
+          decoration: const InputDecoration(
+            labelText: 'Email',
+            icon: Icon(NitoIcons.email),
+          ),
+          keyboardType: TextInputType.emailAddress,
+          autofocus: true,
+          textInputAction: TextInputAction.next,
+        ),
+
+        const NitoGap.heightX2(),
+
+        // パスワード入力欄
+        TextFormField(
+          controller: passwordController,
+          decoration: const InputDecoration(
+            labelText: 'Password',
+            icon: Icon(NitoIcons.password),
+          ),
+          keyboardType: TextInputType.visiblePassword,
+          obscureText: true,
+          textInputAction: TextInputAction.done,
+          onFieldSubmitted: (String? value) => onPasswordFieldSubmitted(),
+        ),
+
+        const NitoGap.heightX4(),
+
+        // サインインボタン
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            onPressed: () => onSignInButtonPressed(),
+            child: Text(isSignInProcessing ? 'サインイン中です…' : 'サインイン'),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- app/feature/auth/lib/src/login/login_page.dart
+++ app/feature/auth/lib/src/login/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:nito_feature_auth/src/login/component/login_form.dart';
 import 'package:nito_feature_auth/src/login/login_page_controller.dart';
 import 'package:nito_styleguide/nito_styleguide.dart';
 
@@ -18,36 +19,16 @@ class LoginPage extends HookConsumerWidget {
         children: [
           const Text('管理アプリはログインが必要です。'),
           const NitoGap.heightX3(),
-          TextFormField(
-            controller: controller.emailController,
-            decoration: const InputDecoration(
-              labelText: 'Email',
-              icon: Icon(NitoIcons.email),
-            ),
-            keyboardType: TextInputType.emailAddress,
-            autofocus: true,
-            textInputAction: TextInputAction.next,
-          ),
-          const NitoGap.heightX2(),
-          TextFormField(
-            controller: controller.passwordController,
-            decoration: const InputDecoration(
-              labelText: 'Password',
-              icon: Icon(NitoIcons.password),
-            ),
-            keyboardType: TextInputType.visiblePassword,
-            obscureText: true,
-            textInputAction: TextInputAction.done,
-            onFieldSubmitted: (String? value) async {
+          LoginForm(
+            isSignInProcessing: state.isLoading,
+            emailController: controller.emailController,
+            passwordController: controller.passwordController,
+            onPasswordFieldSubmitted: () async {
               state.isLoading ? null : await controller.login();
             },
-          ),
-          const NitoGap.heightX4(),
-          ElevatedButton(
-            onPressed: () async {
-              state.isLoading ? null : controller.login();
+            onSignInButtonPressed: () async {
+              state.isLoading ? null : await controller.login();
             },
-            child: Text(state.isLoading ? 'Loading' : 'Sign In'),
           ),
         ],
       ),
