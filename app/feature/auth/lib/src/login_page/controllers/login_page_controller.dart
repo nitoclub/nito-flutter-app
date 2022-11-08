05f7054e6267f070ada29a5117bc49fc8a199631
Ryo Takeuchi <develop@r09i.me>
2022-11-08 11:13:07 +0900

    :bug: autoDispose を一時的に削除
    autoDispose を使用していると実行時にエラーとなってしまうため、一時的に削除しました。

--- app/feature/auth/lib/src/login_page/controllers/login_page_controller.dart
+++ app/feature/auth/lib/src/login_page/controllers/login_page_controller.dart
@@ -4,8 +4,9 @@ import 'package:nito_feature_auth/src/login_page/states/login_page_state.dart';
 import 'package:nito_data_auth/nito_data_auth.dart';
 
 final loginPageController =
-    StateNotifierProvider.autoDispose<LoginPageController, LoginPageState>(
-        (ref) => LoginPageController(ref));
+    StateNotifierProvider<LoginPageController, LoginPageState>(
+  (ref) => LoginPageController(ref),
+);
 
 class LoginPageController extends StateNotifier<LoginPageState> {
   LoginPageController(this.ref) : super(const LoginPageState()) {
