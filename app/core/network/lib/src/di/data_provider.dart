7cc3fe23b39f287f66d73abe6f1af2b002f1e92e
Daichi Aoki <fremie.off@gmail.com>
2022-11-09 23:19:14 +0900

    :sparkles: onAuthStateChange用のProvider追加
    StreamProviderとして追加。
    また、signIn状態をboolで返すProviderも追加。

--- app/core/network/lib/src/di/data_provider.dart
+++ app/core/network/lib/src/di/data_provider.dart
@@ -1,4 +1,5 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart' as rp;
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nito_network/src/authenticator.dart';
 import 'package:nito_network/src/real_authenticator.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -10,12 +11,24 @@ final rp.Provider<SupabaseClient> supabaseClientProvider = rp.Provider(
 
 /// GoTrueClient
 final rp.Provider<GoTrueClient> goTrueClientProvider = rp.Provider(
-  (ref) => ref.read(supabaseClientProvider).auth,
+  (ref) => ref.watch(supabaseClientProvider).auth,
+);
+
+/// AuthState
+final rp.StreamProvider<AuthState> authStateProvider = rp.StreamProvider(
+  (ref) => ref.watch(goTrueClientProvider).onAuthStateChange,
+);
+
+/// IsSignedIn
+final rp.Provider<AsyncValue<bool>> isSignedInProvider = rp.Provider(
+  (ref) => ref
+      .watch(authStateProvider)
+      .whenData((value) => value.session?.user != null),
 );
 
 /// Authenticator
 final rp.Provider<Authenticator> authenticatorProvider = rp.Provider(
   (ref) => RealAuthenticator(
-    client: ref.read(goTrueClientProvider),
+    client: ref.watch(goTrueClientProvider),
   ),
 );
