2bd058efc102183420f4cf3044fb72fc9bce8940
Ryo Takeuchi <develop@r09i.me>
2023-01-11 20:50:36 +0900

    :sparkles: NetworkService クラスを実装

--- packages/core/network/lib/src/authenticator.dart
+++ packages/core/network/lib/src/authenticator.dart
@@ -3,6 +3,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 /// 認証
 abstract class Authenticator {
   Stream<AuthState> get onAuthStateChange;
+  Stream<bool> get isAuthenticated;
 
   Future<AuthResponse> signIn({
     required String email,
