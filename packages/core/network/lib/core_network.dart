4d8e5afca721276a9e223a91f750f385bd4eba58
Ryo Takeuchi <develop@r09i.me>
2023-01-14 08:48:29 +0900

    :recycle: ネットワーク関連クラスのリファクタリング

--- packages/core/network/lib/core_network.dart
+++ packages/core/network/lib/core_network.dart
@@ -1,7 +1,6 @@
 library core_network;
 
-export 'src/di/data_provider.dart';
-
+export 'src/authenticator.dart';
+export 'src/di/network_provider.dart';
 export 'src/participation/participation_api.dart';
 
-export 'src/authenticator.dart';
