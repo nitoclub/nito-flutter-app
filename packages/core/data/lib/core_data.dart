fc023cba0a1d74a99b1dee387e976bc57a3c7c07
Ryo Takeuchi <develop@r09i.me>
2023-01-14 09:27:55 +0900

    :sparkles: スケジュールリポジトリを追加

--- packages/core/data/lib/core_data.dart
+++ packages/core/data/lib/core_data.dart
@@ -1,7 +1,8 @@
 library core_data;
 
 export 'src/auth/auth_repository.dart';
-export 'src/auth/fake_auth_repository.dart';
-
 export 'src/auth/di/data_auth_repository_provider.dart';
 export 'src/auth/di/fake_auth_repository_provider.dart';
+export 'src/auth/fake_auth_repository.dart';
+export 'src/di/repository_provider.dart';
+export 'src/schedule/schedule_repository.dart';
