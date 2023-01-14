130ba632eda051a8c3a55c872fb7a55ce277089d
Ryo Takeuchi <develop@r09i.me>
2023-01-14 21:14:44 +0900

    :recycle: data 層の Provider ファイルをリネーム

--- packages/core/data/lib/core_data.dart
+++ packages/core/data/lib/core_data.dart
@@ -4,6 +4,6 @@ export 'src/auth/auth_repository.dart';
 export 'src/auth/di/data_auth_repository_provider.dart';
 export 'src/auth/di/fake_auth_repository_provider.dart';
 export 'src/auth/fake_auth_repository.dart';
-export 'src/di/repository_provider.dart';
+export 'src/di/data_provider.dart';
 export 'src/repository/participation_repository.dart';
 export 'src/schedule/schedule_repository.dart';
