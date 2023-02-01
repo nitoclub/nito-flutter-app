e425548d1b8ac4eaef0696cf8a9c90f6592baf01
Daichi Aoki <fremie.off@gmail.com>
2023-02-01 22:32:02 +0900

    :recycle: コンストラクタで受け取る値をPrivate化

--- packages/feature/frontend_top/lib/src/view/component/top_schedule_list_tile.dart
+++ packages/feature/frontend_top/lib/src/view/component/top_schedule_list_tile.dart
@@ -3,21 +3,22 @@ import 'package:flutter/material.dart';
 class TopScheduleListTile extends StatelessWidget {
   const TopScheduleListTile({
     super.key,
-    required this.dateString,
-    required this.isNext,
-  });
+    required String dateString,
+    required bool isNext,
+  })  : _dateString = dateString,
+        _isNext = isNext;
 
-  final String dateString;
-  final bool isNext;
+  final String _dateString;
+  final bool _isNext;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      leading: isNext
+      leading: _isNext
           ? const Icon(Icons.schedule)
           : const Icon(Icons.history_toggle_off),
       title: Text(
-        dateString,
+        _dateString,
         style: const TextStyle(
           fontSize: 16,
           fontWeight: FontWeight.bold,
