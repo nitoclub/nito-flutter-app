50bf8d585a054cebe8000e5eb7afefeb22895190
Daichi Aoki <fremie.off@gmail.com>
2023-02-01 20:22:53 +0900

    :recycle: スケジュールのリポジトリ修正
    リポジトリパターンとかいうやつですな。

--- packages/core/data/lib/src/schedule/schedule_repository.dart
+++ packages/core/data/lib/src/schedule/schedule_repository.dart
@@ -1,5 +1,7 @@
+import 'package:core_data/src/schedule/model/schedule.dart';
+
 /// スケジュールリポジトリ
 abstract class ScheduleRepository {
   /// スケジュール一覧を取得する
-  Future<void> fetchSchedules(int scheduleId);
+  Future<List<Schedule>> fetchSchedules();
 }
