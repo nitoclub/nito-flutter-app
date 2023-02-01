d25960e8aab1a744c052514d1b3b85bada4e115c
Daichi Aoki <fremie.off@gmail.com>
2023-02-01 22:16:30 +0900

    :bug: データの取得が可能なように修正
    schedule_apiにsupabaseClientを直差しするように修正
    controllerにawaitつけ忘れてた

--- packages/core/network/lib/src/schedule/supabase_schedule_api.dart
+++ packages/core/network/lib/src/schedule/supabase_schedule_api.dart
@@ -1,32 +1,25 @@
 import 'package:core_network/core_network.dart';
-import 'package:core_network/src/nito_network_service.dart';
 import 'package:core_network/src/schedule/model/network_schedule.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 /// スケジュールAPI を SupabaseClient で実装したクラス
 class SupabaseScheduleApi implements ScheduleApi {
   static const _table = 'schedules';
 
-  final NitoNetworkService _networkService;
+  final SupabaseClient _supabaseClient;
 
-  SupabaseScheduleApi({required NitoNetworkService networkService})
-      : _networkService = networkService;
+  SupabaseScheduleApi({required SupabaseClient supabaseClient})
+      : _supabaseClient = supabaseClient;
 
   @override
-  Future<List<NetworkSchedule>> fetchSchedules() {
-    return _networkService
-        .query(
-      table: _table,
-      action: (builder) => builder
-          .select('*')
-          .is_(NetworkScheduleFields.deletedAt, null)
-          .gte(NetworkScheduleFields.date, DateTime.now())
-          .order(NetworkScheduleFields.date, ascending: true),
-    )
-        .then(
-      (value) {
-        final castedList = value as List<dynamic>;
-        return castedList.map((e) => NetworkSchedule.fromJson(e)).toList();
-      },
-    );
+  Future<List<NetworkSchedule>> fetchSchedules() async {
+    final val = await _supabaseClient
+        .from(_table)
+        .select('*')
+        .is_(NetworkScheduleFields.deletedAt, null)
+        .gte(NetworkScheduleFields.date, DateTime.now())
+        .order(NetworkScheduleFields.date, ascending: true);
+    final castedList = val as List<dynamic>;
+    return castedList.map((e) => NetworkSchedule.fromJson(e)).toList();
   }
 }
--- packages/feature/frontend_top/lib/src/view/controller/top_schedule_list_provider.dart
+++ packages/feature/frontend_top/lib/src/view/controller/top_schedule_list_provider.dart
@@ -2,5 +2,5 @@ import 'package:core_data/core_data.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final topScheduleListProvider = FutureProvider.autoDispose(
-  (ref) async => ref.watch(scheduleRepositoryProvider).fetchSchedules(),
+  (ref) async => await ref.watch(scheduleRepositoryProvider).fetchSchedules(),
 );
