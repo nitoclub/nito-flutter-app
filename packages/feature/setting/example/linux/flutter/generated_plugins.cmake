77aa24c20b49ff81a8842353926cc4f204d6c0b5
Daichi Aoki <fremie.off@gmail.com>
2023-02-15 20:02:49 +0900

    :wrench: setting: pubspec.yamlの更新

--- packages/feature/setting/example/linux/flutter/generated_plugins.cmake
+++ packages/feature/setting/example/linux/flutter/generated_plugins.cmake
@@ -3,7 +3,6 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  feature_setting
   url_launcher_linux
 )
 
--- packages/feature/setting/example/windows/flutter/generated_plugins.cmake
+++ packages/feature/setting/example/windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   app_links
-  feature_setting
   url_launcher_windows
 )
 
