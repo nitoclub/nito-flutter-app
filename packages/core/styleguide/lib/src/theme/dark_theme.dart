4e42bc8aa7698a53f94bec66ade8ee878c59ce88
Daichi Aoki <fremie.off@gmail.com>
2023-02-20 10:19:28 +0900

    :bug: ダークテーマのbrightness設定を修正
    完全に忘れてた。笑

--- packages/core/styleguide/lib/src/theme/dark_theme.dart
+++ packages/core/styleguide/lib/src/theme/dark_theme.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 ThemeData darkTheme(ColorScheme? darkDynamic) {
   final scheme = darkDynamic ??
       // #8d93c8 is pansy
-      ColorScheme.fromSeed(seedColor: const Color(0x004d4398));
+      ColorScheme.fromSeed(
+        seedColor: const Color(0x004d4398),
+        brightness: Brightness.dark,
+      );
   return ThemeData(
     colorScheme: scheme,
   );
