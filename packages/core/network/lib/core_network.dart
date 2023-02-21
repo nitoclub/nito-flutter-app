4fe8819a2da3173815d1cd49f3ef88f90b82a09a
Daichi Aoki <fremie.off@gmail.com>
2023-02-21 22:20:02 +0900

    :wrench: ログインユーザーをFetchするProviderをExport

--- packages/core/network/lib/core_network.dart
+++ packages/core/network/lib/core_network.dart
@@ -5,3 +5,4 @@ export 'src/di/network_provider.dart';
 export 'src/participation/participation_api.dart';
 export 'src/schedule/schedule_api.dart';
 export 'src/member/member_api.dart';
+export 'src/member/supabase_current_member_fetch_api.dart';
