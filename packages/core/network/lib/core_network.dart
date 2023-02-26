e5b1e8e05136b911d7a6a1a2051a7297a0501b19
Daichi Aoki <fremie.off@gmail.com>
2023-02-26 22:51:21 +0900

    :sparkles: 参加登録用のProviderを追加/Export

--- packages/core/network/lib/core_network.dart
+++ packages/core/network/lib/core_network.dart
@@ -3,6 +3,7 @@ library core_network;
 export 'src/authenticator.dart';
 export 'src/di/network_provider.dart';
 export 'src/participation/participation_api.dart';
+export 'src/participation/supabase_participation_register_api.dart';
 export 'src/schedule/schedule_api.dart';
 export 'src/member/member_api.dart';
 export 'src/member/supabase_member_fetch_api.dart';
---
+++ packages/core/network/lib/src/participation/supabase_participation_register_api.dart
@@ -0,0 +1,16 @@
+import 'package:core_model/core_model.dart';
+import 'package:core_network/src/di/network_provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final supabaseParticipationRegisterApi =
+    FutureProvider.family<void, ParticipationRegisterArgs>(
+  (ref, args) async {
+    const table = 'participation';
+    final supabaseClient = ref.watch(supabaseClientProvider);
+    // 参加を登録する
+    await supabaseClient.from(table).insert({
+      "schedule_id": args.scheduleId,
+      "member_id": args.memberId,
+    });
+  },
+);
