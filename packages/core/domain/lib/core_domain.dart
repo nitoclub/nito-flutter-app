0ec6ee33719a86668fc898bce4112412067ab942
Daichi Aoki <fremie.off@gmail.com>
2023-03-01 21:56:34 +0900

    :recycle: 消したファイルのexportを削除

--- packages/core/domain/lib/core_domain.dart
+++ packages/core/domain/lib/core_domain.dart
@@ -1,4 +1,1 @@
 library core_domain;
-
-export 'src/fetch_participation_schedules_usecase.dart';
-export 'src/participate_usecase.dart';
--- packages/core/model/lib/core_model.dart
+++ packages/core/model/lib/core_model.dart
@@ -1,5 +1,4 @@
 library core_model;
 
 export 'src/member/member.dart';
-export 'src/participation/participation_register_args.dart';
 export 'src/schedule/schedule.dart';
