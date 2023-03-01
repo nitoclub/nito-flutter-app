a8eff1ad7d5deed40b856f4eb89c6f407745f99e
Daichi Aoki <fremie.off@gmail.com>
2023-03-01 21:50:00 +0900

    :wastebasket: participateのゴミ

--- packages/core/data/lib/src/participation/di/participation_register_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:core_model/core_model.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final participationRegisterProvider =
-    FutureProvider.autoDispose.family<void, ParticipationRegisterArgs>(
-  (ref, arg) async => UnimplementedError(),
-);
--- packages/core/data/lib/src/participation/participation_repository.dart
+++
@@ -1,8 +0,0 @@
-/// 参加情報リポジトリ
-abstract class ParticipationRepository {
-  /// 参加者一覧を取得する
-  Future<void> fetchParticipants(int scheduleId);
-
-  /// 該当の予定日に参加する
-  Future<void> participate(int scheduleId, int memberId);
-}
