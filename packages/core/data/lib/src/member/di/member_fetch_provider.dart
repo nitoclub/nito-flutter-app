81939797a771bf493633eb215e22a98b2a17faa5
Daichi Aoki <fremie.off@gmail.com>
2023-03-01 21:52:03 +0900

    :wastebasket: Network側に移したため、ゴミになった

--- packages/core/data/lib/src/member/di/member_fetch_provider.dart
+++
@@ -1,6 +0,0 @@
-import 'package:core_model/core_model.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final memberFetchProvider = FutureProvider.autoDispose<Member>(
-  (ref) => throw UnimplementedError(),
-);
