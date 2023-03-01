7381e22180e499dc90308b49b4617b53ae6c66db
Daichi Aoki <fremie.off@gmail.com>
2023-03-01 20:39:30 +0900

    :recycle: [Network] exportを修正

--- packages/core/network/lib/core_network.dart
+++ packages/core/network/lib/core_network.dart
@@ -2,8 +2,7 @@ library core_network;
 
 export 'src/authenticator.dart';
 export 'src/di/network_provider.dart';
+export 'src/di/supabase_override_network_provider.dart';
 export 'src/participation/participation_api.dart';
-export 'src/participation/supabase_participation_register_api.dart';
 export 'src/schedule/schedule_api.dart';
 export 'src/member/member_api.dart';
-export 'src/member/supabase_member_fetch_api.dart';
