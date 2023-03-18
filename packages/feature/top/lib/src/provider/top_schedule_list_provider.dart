7eb92510bba902ff56481c51155b3275f33855bf
Daichi Aoki <fremie.off@gmail.com>
2023-03-18 20:32:46 +0900

    :recycle: 直近の予定を取得するProviderに修正

--- packages/feature/top/lib/src/provider/top_schedule_list_provider.dart
+++
@@ -1,8 +0,0 @@
-import 'package:core_data/core_data.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
-
-final topScheduleListProvider = FutureProvider.autoDispose(
-  (ref) async => await ref
-      .watch(legacyScheduleRepositoryProvider)
-      .fetchUpcomingSchedules(),
-);
