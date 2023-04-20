d445c0885eae3e4855c7e2bb343ac95fa392d750
Daichi Aoki <fremie.off@gmail.com>
2023-04-20 19:47:39 +0900

    :recycle: ルーター設定をbuilder形式に修正(#131)

---
+++ apps/frontend/lib/src/router/router.dart
@@ -0,0 +1,56 @@
+import 'package:feature_auth/feature_auth.dart';
+import 'package:feature_schedule/feature_schedule.dart';
+import 'package:feature_settings/feature_settings.dart';
+import 'package:feature_top/feature_top.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+part 'router.g.dart';
+
+/// ルート
+@TypedGoRoute<RootRoute>(
+  path: '/',
+)
+class RootRoute extends GoRouteData {
+  const RootRoute();
+
+  @override
+  build(BuildContext context, GoRouterState state) => TopPage(
+        onSettingsActionPressed: (BuildContext context) {
+          context.push('/settings');
+        },
+      );
+}
+
+/// ログインページルート
+@TypedGoRoute<LoginRoute>(
+  path: '/login',
+)
+class LoginRoute extends GoRouteData {
+  const LoginRoute();
+
+  @override
+  build(BuildContext context, GoRouterState state) => const LoginPage();
+}
+
+/// セッティングページルート
+@TypedGoRoute<SettingsRoute>(
+  path: '/settings',
+)
+class SettingsRoute extends GoRouteData {
+  const SettingsRoute();
+
+  @override
+  build(BuildContext context, GoRouterState state) => const SettingsScreen();
+}
+
+/// スケジュールページルート
+@TypedGoRoute<ScheduleRoute>(
+  path: '/schedule',
+)
+class ScheduleRoute extends GoRouteData {
+  const ScheduleRoute();
+
+  @override
+  build(BuildContext context, GoRouterState state) => const SchedulePage();
+}
