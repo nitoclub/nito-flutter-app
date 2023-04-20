d92cb4f77ff31d648a6d3259cd6417dbabb1ba98
Daichi Aoki <fremie.off@gmail.com>
2023-04-20 19:58:08 +0900

    :recycle: ルーターProviderをbuilder形式に修正(#131)

---
+++ apps/frontend/lib/src/router/router_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$routerHash() => r'a9570b8694051317b230db5ffd762bf965db5214';
+
+/// ルーティング設定のプロバイダー
+///
+/// Copied from [router].
+@ProviderFor(router)
+final routerProvider = AutoDisposeProvider<GoRouter>.internal(
+  router,
+  name: r'routerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$routerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RouterRef = AutoDisposeProviderRef<GoRouter>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
