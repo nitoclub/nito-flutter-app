7de2977d67864cac35526eaa3ebb981e5b702534
Daichi Aoki <fremie.off@gmail.com>
2023-05-13 22:16:47 +0900

    :+1: nito_iconに名前用のアイコンを追加(#170)

--- packages/core/styleguide/lib/src/icon/nito_icons.dart
+++ packages/core/styleguide/lib/src/icon/nito_icons.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class NitoIcons {
   static const IconData email = Icons.email_outlined;
   static const IconData password = Icons.password_outlined;
+  static const IconData name = Icons.person_outline;
 }
